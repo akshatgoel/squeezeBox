b0b04dbf43be14c87736092e7f6f486d0c33822f
Elijah Madden <elijah.madden@nikkoam.com>
2014-08-19 10:27:10 +0900

    Perform defaults/options merge in a single deep-extend operation, configurable animation settings, slightly better compressed size, etc.

--- javascripts/squeezeBox.js
+++ javascripts/squeezeBox.js
@@ -7,100 +7,84 @@
 **
 */
 
-(function( $ ) {
-  $.fn.squeezeBox = function(options) {
-	
+(function($) {
+  $.fn.squeezeBox = function (options) {
+
     var el = this;
 
-	
 	var docHeight = $(document).height();
 	var docWidth = $(document).width();
-	
-	// Options initialization
-	if(!options){
-		options = {overlay: '',container: '',data: '',close: ''};
-	}
-	
-	if(!options.overlay){ 
-		options.overlay = '';
-	}
-	
-	if(!options.container){
-		options.container = '';
-	}
-	
-	if(!options.data){
-		options.data = '';
-	}
-	
-	if(!options.close){
-		options.close = '';
-	}
-	
-	// Create some defaults, extending them with any options that were provided
-    var overlayCSS = $.extend( {
-		'width': docWidth+'px',
-		'height': docHeight+'px',
-		'position': 'absolute',
-		'z-index': '999',
-		'top': '0',
-		'left': '0',
-		'background': 'rgba(64,64,64,0.6)'
-    }, options.overlay);
-	
-	var containerCSS = $.extend( {
-		'width': '100%',
-		'z-index': '1000',
-		'margin': '0',
-		'padding': '20px',
-		'background': '#fff',
-		'border': '1px solid #efefef'
-    }, options.container);
-	
-	var closeCSS = $.extend({
-		'float': 'right',
-		'font-weight': 'bolder',
-		'cursor': 'pointer'
-	},options.close);
-	
-	var dataCSS = $.extend({
-	
-	},options.data);
-	
-  	var text = el.html();
-	
-  	var overlay = '<div class="squeezeBox-overlay"></div>';
-		
-  	el.html(overlay);
 
-	$('.squeezeBox-overlay').css(overlayCSS);
-	
-  	var boxHtml = '<div class="squeezeBox-container" >';
-  	boxHtml = boxHtml + '<span class="squeezeBox-close">X</span><br style="clear:both;"/>';
-  	boxHtml = boxHtml + '<div class="squeezeBox-data" >'+text+'</div></div>';
+	var defaults = {
+		classPrefix: 'squeezeBox-',
+		overlay: {
+			width: docWidth + 'px',
+			height: docHeight + 'px',
+			position: 'absolute',
+			zIndex: 999,
+			top: 0,
+			left: 0,
+			background: 'rgba(64,64,64,0.6)'
+		},
+		container: {
+			width: '100%',
+			margin: 0,
+			padding: '20px',
+			background: '#fff',
+			border: '1px solid #efefef'
+		},
+		close: {
+			float: 'right',
+			clear: 'both',
+			fontWeight: 'bolder',
+			cursor: 'pointer'
+		},
+		data: {
+		},
+		animation: {
+			properties: {
+		  		width: '80%',
+		  		maxHeight: '80%',
+		  		overflow: 'auto',
+		  		marginTop: '10%',
+		  		marginLeft: '10%'
+			},
+			options: {
+				duration: 700
+			}
+		}
+	};
 
-  	$('.squeezeBox-overlay').html(boxHtml);
-	$('.squeezeBox-container').css(containerCSS);
-	$('.squeezeBox-close').css(closeCSS);
-	$('.squeezeBox-data').css(dataCSS);
-	
+	var opts = $.extend(true, {}, defaults, options);
 
-  	el.show(0,function(){
-  		$('.squeezeBox-container').animate({
-	  		width: '80%',
-	  		maxHeight: '80%',
-	  		overflow: 'auto',
-	  		marginTop: '10%',
-	  		marginLeft: '10%'
-	  	},700);
-  	})
-  	
-  	$('.squeezeBox-close').click(function(){
-  		el.hide();
-  		el.html(text);
+    var prefix = opts.classPrefix;
 
+  	var original = el.html();
+
+  	el.html('<div class="' + prefix + 'overlay"></div>');
+
+	$('.' + prefix + 'overlay', el)
+		.css(opts.overlay)
+		.html([
+			'<div class="' + prefix + 'container" >',
+			'<div class="' + prefix + 'close">X</div>',
+			'<div class="' + prefix + 'data" >', original, '</div>',
+			'</div>'
+		].join(''));
+
+	$('.' + prefix + 'container', el).css(opts.container);
+	$('.' + prefix + 'close', el).css(opts.close);
+	$('.' + prefix + 'data', el).css(opts.data);
+
+
+  	el.show(0, function () {
+  		$('.' + prefix + 'container').animate(opts.animation.properties, opts.animation.options);
   	});
 
+  	$('.' + prefix + 'close').click(function () {
+  		el.hide().html(original);
+  	});
 
   };
-})( jQuery );
+
+}(jQuery));
